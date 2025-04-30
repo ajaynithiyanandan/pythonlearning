3aaa5781a6f272658fe0739ea2fe84077ceec5d8
Ajay Nithiyanandan <ajay.nithiyanandan89@gmail.com>
2025-04-30 03:31:01 -0600

    Added some more code

---
+++ ArithmeticOperations.py
@@ -0,0 +1,17 @@
+# +, -, *, / - Basic arithmetic operations
+# ** (Exponent), // (Double Division - Returns int part of result), % (Mod - Returns remainder) - Advanced arithmetic operations
+# Precedence of operators: () , **, /,*,//, %, + , -
+
+print("Arithmetic Operations")
+x=input('Enter first number: ')
+y=input('Enter second number: ')
+x=int(x) # Convert string to int
+y=int(y) # Convert string to int
+print('x+y=',x+y) # Addition
+print('x-y=',x-y) # Subtraction
+print('x*y=',x*y) # Multiplication
+print('x/y=',x/y) # Division
+print('x**y=',x**y) # Exponentiation
+print('x//y=',x//y) # Double Division - Returns int part of result
+print('x%y=',x%y) # Mod - Returns remainder
+print('x-y=',x-y) # Subtraction
